cf060e95e5fbd66b85878cb223256ed8a09d743e
Oli Elliott <oliver.elliott@ft.com>
2019-08-02 16:31:58 +0100

    theme updates. closeish

--- R/ft_theme.R
+++ R/ft_theme.R
@@ -2,86 +2,57 @@
 #' @param base_size The base font size
 #' @param base_family Font family
 #' @export
-ft_theme <- function(base_size = 12, base_family = "") {
+ft_theme <- function(base_size = 12,
+                     base_family = "",
+                     base_line_size = base_size / 170,
+                     base_rect_size = base_size / 170) {
+
   half_line <- base_size/2
-  ggplot2::theme(
-    line = ggplot2::element_line(colour = "black",
-                        size = 0.5,
-                        linetype = 1,
-                        lineend = "butt"),
+  grid_line_color <- ft_colors("black-20")
+  grid_line_size <- 0.2
+  title_text_color <- ft_colors("black")
+  other_text_color <- ft_colors("black-50")
 
-    rect = ggplot2::element_rect(fill = "white",
-                        colour = "black",
-                        size = 0.5,
-                        linetype = 1),
+  ggplot2::theme_minimal(base_size = base_size,
+                         base_family = base_family,
+                         base_line_size = base_line_size) %+replace%
+    ggplot2::theme(
+      plot.title = ggplot2::element_text(
+        color = title_text_color,
+        face = "bold",
+        hjust = 0
+      ),
+      axis.title = ggplot2::element_text(
+        color = other_text_color,
+        size = ggplot2::rel(0.9),
+        face = "bold"
+      ),
+      axis.text = ggplot2::element_text(
+        color = other_text_color,
+        size = ggplot2::rel(0.8)
+      ),
+      axis.line.x = ggplot2::element_line(
+        colour = grid_line_color,
+        size = grid_line_size
+      ),
+      panel.grid.major.y = ggplot2::element_line(
+        color = grid_line_color,
+        size = grid_line_size
+      ),
+      panel.grid.major.x = ggplot2::element_blank(),
+      panel.grid.minor = ggplot2::element_blank(),
+      legend.position = "top",
+      legend.justification = c(0,0),
+      legend.direction = "horizontal",
+      legend.title = ggplot2::element_blank(),
+      legend.spacing.x = ggplot2::unit(1, "char"),
+      legend.text = ggplot2::element_text(
+        color = other_text_color,
+        hjust = 0,
+        size = ggplot2::rel(0.8)
+      ),
 
-    text = ggplot2::element_text(family = base_family,
-                        face = "plain",
-                        colour = "black",
-                        size = base_size,
-                        lineheight = 0.9,
-                        hjust = 0.5,
-                        vjust = 0.5,
-                        angle = 0,
-                        margin = ggplot2::margin(),
-                        debug = FALSE),
-
-    axis.line =         ggplot2::element_line(),
-    axis.line.x =       ggplot2::element_blank(),
-    axis.line.y =       ggplot2::element_blank(),
-    axis.text =         ggplot2::element_text(size = ggplot2::rel(0.9),
-                                     colour = "#b0b0b0",
-                                     face = "bold"),
-    axis.text.x =       ggplot2::element_text(margin = ggplot2::margin(t = 0.8 * half_line/2),
-                                     vjust = 1),
-    axis.text.y =       ggplot2::element_text(margin = ggplot2::margin(l = 0.8 * half_line/2),
-                                     hjust = 1),
-    axis.ticks =        ggplot2::element_line(colour = "#b0b0b0"),
-    axis.ticks.length = ggplot2::unit(half_line/2, "pt"),
-    axis.title.x =      ggplot2::element_text(margin = ggplot2::margin(t = 0.8 * half_line, b = 0.8 * half_line/2)),
-    axis.title.y =      ggplot2::element_text(angle = 90,
-                                     margin = ggplot2::margin(r = 0.8 * half_line, l = 0.8 * half_line/2)),
-
-    legend.background =    ggplot2::element_rect(colour = NA),
-    legend.margin =        ggplot2::unit(0.2,"cm"),
-    legend.key =           ggplot2::element_rect(fill = "grey95",
-                                        colour = "white"),
-    legend.key.size =      ggplot2::unit(1.2, "lines"),
-    legend.key.height =    NULL,
-    legend.key.width =     NULL,
-    legend.text =          ggplot2::element_text(size = ggplot2::rel(0.8)),
-    legend.text.align =    NULL,
-    legend.title =         ggplot2::element_text(hjust = 0),
-    legend.title.align =   NULL,
-    legend.position =      "right",
-    legend.direction =     NULL,
-    legend.justification = "center",
-    legend.box =           NULL,
-
-    panel.background =     ggplot2::element_blank(),
-    panel.border =         ggplot2::element_blank(),
-    panel.grid.major =     ggplot2::element_line(color = "#b0b0b0"),
-    panel.grid.minor =     ggplot2::element_blank(),
-    panel.margin =         ggplot2::unit(half_line, "pt"),
-    panel.margin.x =       NULL,
-    panel.margin.y =       NULL,
-    panel.ontop =          FALSE,
-
-    strip.background = ggplot2::element_rect(fill = "grey85",
-                                    colour = NA),
-    strip.text = ggplot2::element_text(colour = "grey10",
-                              size = ggplot2::rel(0.8)),
-    strip.text.x = ggplot2::element_text(margin = ggplot2::margin(t = half_line, b = half_line)),
-    strip.text.y = ggplot2::element_text(angle = -90,
-                                margin = ggplot2::margin(l = half_line, r = half_line)),
-    strip.switch.pad.grid = ggplot2::unit(0.1, "cm"),
-    strip.switch.pad.wrap = ggplot2::unit(0.1, "cm"),
-
-    plot.background = ggplot2::element_blank(),
-    plot.title = ggplot2::element_text(size = ggplot2::rel(1.2), margin = ggplot2::margin(b = half_line * 1.2)),
-    plot.margin = ggplot2::margin(half_line, half_line, half_line, half_line),
-
-    complete = TRUE
-  )
+      complete = TRUE
+    )
 }
 
